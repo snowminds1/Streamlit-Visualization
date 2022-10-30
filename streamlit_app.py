f9e87d8d685dbad748dec408e4993c306b404302
snowminds1 <116988012+snowminds1@users.noreply.github.com>
2022-10-30 10:40:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,13 @@ import streamlit as st
 from snowflake.snowpark import Session
 
 
-st.write("My cool secrets:", st.secrets["snowflake"]["account"])
+st.write("My cool account:", st.secrets["snowflake"]["account"])
+st.write("My cool user:", st.secrets["snowflake"]["user"])
+st.write("My cool password:", st.secrets["snowflake"]["password"])
+st.write("My cool role:", st.secrets["snowflake"]["role"])
+st.write("My cool warehouse:", st.secrets["snowflake"]["warehouse"])
+st.write("My cool database:", st.secrets["snowflake"]["database"])
+st.write("My cool schema:", st.secrets["snowflake"]["schema"])
 
 st.title('Uber pickups in NYC')
 connection_parameters = {
@@ -15,7 +21,7 @@ connection_parameters = {
     "schema": st.secrets["snowflake"]["schema"],
 }
 
-test_session = Session.builder.configs(connection_parameters).create()
-df_table = test_session.sql("SELECT  TYPE_TRAN,SUM(AMOUNT) AS AMOUNT_IN_MILLION from D_FIN_STG.FIN_STG.TRANS_DTA group by 1 ").collect()
-df = pd.DataFrame(df_table)
-print(df)
+# test_session = Session.builder.configs(connection_parameters).create()
+# df_table = test_session.sql("SELECT  TYPE_TRAN,SUM(AMOUNT) AS AMOUNT_IN_MILLION from D_FIN_STG.FIN_STG.TRANS_DTA group by 1 ").collect()
+# df = pd.DataFrame(df_table)
+# print(df)
