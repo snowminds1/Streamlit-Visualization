70aa1f70d965c9af765a9cd5d1712acf289f721c
snowminds1 <116988012+snowminds1@users.noreply.github.com>
2022-10-30 10:29:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,18 @@ def init_connection():
 
 conn = init_connection()
 
-st.title('Uber pickups in NYC')
+
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT  TYPE_TRAN,SUM(AMOUNT) AS AMOUNT_IN_MILLION from D_FIN_STG.FIN_STG.TRANS_DTA group by 1")
+df = pd.DataFrame(df_table)
+print(df)
+
+#st.title('Uber pickups in NYC')
 # connection_parameters = {
 #     "account": "dh04284.ap-southeast-1",
 #     "user": "SNOWMINDS",
@@ -27,46 +38,46 @@ st.title('Uber pickups in NYC')
 #     "schema": "FIN_WBG"
 # }
 
-test_session = Session.builder.configs(conn).create()
-df_table = test_session.sql("SELECT  TYPE_TRAN,SUM(AMOUNT) AS AMOUNT_IN_MILLION from D_FIN_STG.FIN_STG.TRANS_DTA group by 1 ").collect()
-df = pd.DataFrame(df_table)
-print(df)
-#st.sidebar.title("Custom Filters")
+# test_session = Session.builder.configs(conn).create()
+# df_table = test_session.sql("SELECT  TYPE_TRAN,SUM(AMOUNT) AS AMOUNT_IN_MILLION from D_FIN_STG.FIN_STG.TRANS_DTA group by 1 ").collect()
+# df = pd.DataFrame(df_table)
+# print(df)
+# #st.sidebar.title("Custom Filters")
 
 
-with st.container():
-   st.write("Custom Filters")
-   st.columns(3)
-   col1, col2, col3 = st.columns(3)
+# with st.container():
+#    st.write("Custom Filters")
+#    st.columns(3)
+#    col1, col2, col3 = st.columns(3)
 
-   with col1:
-       st.header("Select Sender")
-       df_table1 = test_session.sql("SELECT SENDER from D_FIN_STG.FIN_STG.TRANS_DTA group by 1 ").collect()
-       df1 = pd.DataFrame(df_table1)
-       select = st.selectbox('Select a State', df1['SENDER'],key='select1')
-       st.write('You selected:', select)
-       df_table3 = test_session.sql(
-           "SELECT  TYPE_TRAN,SUM(AMOUNT) AS AMOUNT_IN_MILLION from D_FIN_STG.FIN_STG.TRANS_DTA where sender= '"+select+ "' group by 1 ").collect()
-       df3 = pd.DataFrame(df_table3)
-       st.bar_chart(data=df3, x="TYPE_TRAN", y="AMOUNT_IN_MILLION", width=0, height=0, use_container_width=True)
+#    with col1:
+#        st.header("Select Sender")
+#        df_table1 = test_session.sql("SELECT SENDER from D_FIN_STG.FIN_STG.TRANS_DTA group by 1 ").collect()
+#        df1 = pd.DataFrame(df_table1)
+#        select = st.selectbox('Select a State', df1['SENDER'],key='select1')
+#        st.write('You selected:', select)
+#        df_table3 = test_session.sql(
+#            "SELECT  TYPE_TRAN,SUM(AMOUNT) AS AMOUNT_IN_MILLION from D_FIN_STG.FIN_STG.TRANS_DTA where sender= '"+select+ "' group by 1 ").collect()
+#        df3 = pd.DataFrame(df_table3)
+#        st.bar_chart(data=df3, x="TYPE_TRAN", y="AMOUNT_IN_MILLION", width=0, height=0, use_container_width=True)
 
-   with col2:
-       st.header("Select receiver")
-       df_table2 = test_session.sql("SELECT SENDER from D_FIN_STG.FIN_STG.TRANS_DTA group by 1 ").collect()
-       df2 = pd.DataFrame(df_table2)
-       select2 = st.selectbox('Select a State', df2['SENDER'],key='select2')
-       st.write('You selected:', select2)
-       df_table3 = test_session.sql(
-           "SELECT  TYPE_TRAN,SUM(AMOUNT) AS AMOUNT_IN_MILLION from D_FIN_STG.FIN_STG.TRANS_DTA group by 1 ").collect()
-       df3 = pd.DataFrame(df_table3)
-       st.bar_chart(data=df3, x="TYPE_TRAN", y="AMOUNT_IN_MILLION", width=0, height=0, use_container_width=True)
+#    with col2:
+#        st.header("Select receiver")
+#        df_table2 = test_session.sql("SELECT SENDER from D_FIN_STG.FIN_STG.TRANS_DTA group by 1 ").collect()
+#        df2 = pd.DataFrame(df_table2)
+#        select2 = st.selectbox('Select a State', df2['SENDER'],key='select2')
+#        st.write('You selected:', select2)
+#        df_table3 = test_session.sql(
+#            "SELECT  TYPE_TRAN,SUM(AMOUNT) AS AMOUNT_IN_MILLION from D_FIN_STG.FIN_STG.TRANS_DTA group by 1 ").collect()
+#        df3 = pd.DataFrame(df_table3)
+#        st.bar_chart(data=df3, x="TYPE_TRAN", y="AMOUNT_IN_MILLION", width=0, height=0, use_container_width=True)
 
 
-"Energy Costs By Month"
-st.bar_chart(data=df, x="TYPE_TRAN", y="AMOUNT_IN_MILLION", width=0, height=0, use_container_width=True)
+# "Energy Costs By Month"
+# st.bar_chart(data=df, x="TYPE_TRAN", y="AMOUNT_IN_MILLION", width=0, height=0, use_container_width=True)
 
-"Energy Costs By Month"
-st.bar_chart(data=df, x="TYPE_TRAN", y="AMOUNT_IN_MILLION", width=0, height=0, use_container_width=True)
+# "Energy Costs By Month"
+# st.bar_chart(data=df, x="TYPE_TRAN", y="AMOUNT_IN_MILLION", width=0, height=0, use_container_width=True)
 # d = alt.Chart(df).mark_bar().encode(
 #     x='TYPE_TRAN',
 #     y='AMOUNT',
