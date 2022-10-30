9f3b53ca4e2ab7c1f368ac7381418d68a68c4a50
snowminds1 <116988012+snowminds1@users.noreply.github.com>
2022-10-30 10:43:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,37 @@ test_session = Session.builder.configs(connection_parameters).create()
 df_table = test_session.sql("SELECT  TYPE_TRAN,SUM(AMOUNT) AS AMOUNT_IN_MILLION from D_FIN_STG.FIN_STG.TRANS_DTA group by 1 ").collect()
 df = pd.DataFrame(df_table)
 print(df)
+
+with st.container():
+   st.write("Custom Filters")
+   st.columns(3)
+   col1, col2, col3 = st.columns(3)
+
+   with col1:
+       st.header("Select Sender")
+       df_table1 = test_session.sql("SELECT SENDER from D_FIN_STG.FIN_STG.TRANS_DTA group by 1 ").collect()
+       df1 = pd.DataFrame(df_table1)
+       select = st.selectbox('Select a State', df1['SENDER'],key='select1')
+       st.write('You selected:', select)
+       df_table3 = test_session.sql(
+           "SELECT  TYPE_TRAN,SUM(AMOUNT) AS AMOUNT_IN_MILLION from D_FIN_STG.FIN_STG.TRANS_DTA where sender= '"+select+ "' group by 1 ").collect()
+       df3 = pd.DataFrame(df_table3)
+       st.bar_chart(data=df3, x="TYPE_TRAN", y="AMOUNT_IN_MILLION", width=0, height=0, use_container_width=True)
+
+   with col2:
+       st.header("Select receiver")
+       df_table2 = test_session.sql("SELECT SENDER from D_FIN_STG.FIN_STG.TRANS_DTA group by 1 ").collect()
+       df2 = pd.DataFrame(df_table2)
+       select2 = st.selectbox('Select a State', df2['SENDER'],key='select2')
+       st.write('You selected:', select2)
+       df_table3 = test_session.sql(
+           "SELECT  TYPE_TRAN,SUM(AMOUNT) AS AMOUNT_IN_MILLION from D_FIN_STG.FIN_STG.TRANS_DTA group by 1 ").collect()
+       df3 = pd.DataFrame(df_table3)
+       st.bar_chart(data=df3, x="TYPE_TRAN", y="AMOUNT_IN_MILLION", width=0, height=0, use_container_width=True)
+
+
+"Energy Costs By Month"
+st.bar_chart(data=df, x="TYPE_TRAN", y="AMOUNT_IN_MILLION", width=0, height=0, use_container_width=True)
+
+"Energy Costs By Month"
+st.bar_chart(data=df, x="TYPE_TRAN", y="AMOUNT_IN_MILLION", width=0, height=0, use_container_width=True)
