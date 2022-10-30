da242f4967dc9dd5f44e8a5769d9145587949b6e
snowminds1 <116988012+snowminds1@users.noreply.github.com>
2022-10-30 21:37:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,7 +97,7 @@ with st.container():
         st.write("No Data Found")
     else:
         d = alt.Chart(df).mark_bar().encode(x='BANK', y='TOTAL_AMOUNT', tooltip=['BANK', 'TOTAL_AMOUNT'],
-                                            color=alt.value('#3498db')).properties(height=400, width=1024)
+                                            color=alt.value('#3498db')).properties(height=400, width=1200)
         st.altair_chart(d)
 
 with st.container():
