289bcaac1cdad102a970a0b3feb9793d0ed73efa
snowminds1 <116988012+snowminds1@users.noreply.github.com>
2022-11-01 13:19:02 +0530

    streamlit_app.py v2.0

--- streamlit_app.py
+++ streamlit_app.py
@@ -99,14 +99,17 @@ with st.container():
 
     df_metric1 = test_session.sql("SELECT SUM(AMOUNT) AS TXN_AMOUNT from D_FIN_STG.FIN_STG.TRANS_DTA where AMOUNT is not null and BANK is not null and (TYPE_TRAN= '" + payment_type_select + "' or '" + payment_type_select + "' ='All') and (SENDER= '" + sender_select + "' or '" + sender_select + "' ='All') and (RECEIVER= '" + receiver_select + "' or '"+ receiver_select +"' ='All') and (BANK= '" + bank_select + "' or '"+ bank_select +"' ='All') and   TRANS_DATE::DATE between '" + str(
                 from_dt) + "' and '" + str(to_dt) + "'").collect()
-    df1 = pd.DataFrame(df_metric1,index=None)
+    df1 = pd.DataFrame(df_metric1)
     res = df1['TXN_AMOUNT'].loc[df1.index[0]]
-    currency = str(u'\u20B9')
 
-    kpi3.metric(
+    if res==None:
+      kpi3.metric(
         label="Transaction Amount",
-        value=currency + " "+numerize.numerize(float(res))
-    )
+        value = 0)
+    else:
+        kpi3.metric(
+            label="Transaction Amount",
+            value=currency + " " + numerize.numerize(float(res)))
 
 
 with st.container():
@@ -118,33 +121,32 @@ with st.container():
    with col1:
         st.subheader("Payment Type vs Transaction Amount")
         df_table3 = test_session.sql(
-            "SELECT  TYPE_TRAN,SUM(AMOUNT) AS AMOUNT,SUM(AMOUNT) AS AMOUNT_IN_WORDS,COUNT(TYPE_TRAN) AS TXN_COUNT from D_FIN_STG.FIN_STG.TRANS_DTA where (TYPE_TRAN= '" + payment_type_select + "' or '" + payment_type_select + "' ='All') and (SENDER= '" + sender_select + "' or '" + sender_select + "' ='All') and (RECEIVER= '" + receiver_select + "' or '"+ receiver_select +"' ='All') and (BANK= '" + bank_select + "' or '"+ bank_select +"' ='All') and   TRANS_DATE::DATE between '" + str(
+            "SELECT  TYPE_TRAN as TRANSACTION_TYPE,SUM(AMOUNT) AS AMOUNT,SUM(AMOUNT) AS TXN_AMOUNT,COUNT(TYPE_TRAN) AS TXN_COUNT from D_FIN_STG.FIN_STG.TRANS_DTA where (TYPE_TRAN= '" + payment_type_select + "' or '" + payment_type_select + "' ='All') and (SENDER= '" + sender_select + "' or '" + sender_select + "' ='All') and (RECEIVER= '" + receiver_select + "' or '"+ receiver_select +"' ='All') and (BANK= '" + bank_select + "' or '"+ bank_select +"' ='All') and   TRANS_DATE::DATE between '" + str(
                 from_dt) + "' and '" + str(to_dt) + "' group by 1 ").collect()
         df3 = pd.DataFrame(df_table3)
-
-        for ind in df3.index:
-            df3['AMOUNT_IN_WORDS'][ind] = numerize.numerize(float(df3['AMOUNT_IN_WORDS'][ind]))
 
         if df3.empty:
             st.write("No Data Found")
         else:
+            for ind in df3.index:
+                df3['TXN_AMOUNT'][ind] = currency + "" + numerize.numerize(float(df3['TXN_AMOUNT'][ind]))
             d = alt.Chart(df3).mark_arc(innerRadius=50).encode(theta=alt.Theta(field="AMOUNT", type="quantitative"),
-                                                      color=alt.Color(field="TYPE_TRAN", type="nominal"), tooltip=['TYPE_TRAN','AMOUNT_IN_WORDS','TXN_COUNT'])
+                                                      color=alt.Color(field="TRANSACTION_TYPE", type="nominal"), tooltip=['TRANSACTION_TYPE','TXN_AMOUNT','TXN_COUNT'])
             st.altair_chart(d)
 
    with col2:
         st.subheader("Transactions Count")
         df_table3 = test_session.sql(
-            "SELECT  COUNT(TYPE_TRAN) AS TXN_COUNT, DAY(TRANS_DATE::DATE) AS TRANS_DATE,DAY(TRANS_DATE::DATE)||'/'||MONTH(TRANS_DATE::DATE) AS TRANS_DAT from D_FIN_STG.FIN_STG.TRANS_DTA where (TYPE_TRAN= '" + payment_type_select + "' or '" + payment_type_select + "' ='All') and (SENDER= '" + sender_select + "' or '" + sender_select + "' ='All') and (RECEIVER= '" + receiver_select + "' or '"+ receiver_select +"' ='All') and (BANK= '" + bank_select + "' or '"+ bank_select +"' ='All') and   TRANS_DATE::DATE between '" + str(
+            "SELECT  COUNT(TYPE_TRAN) AS TXN_COUNT, DAY(TRANS_DATE::DATE) AS TRANSACTION_DATE,DAY(TRANS_DATE::DATE)||'/'||MONTH(TRANS_DATE::DATE) AS TXN_DATE from D_FIN_STG.FIN_STG.TRANS_DTA where (TYPE_TRAN= '" + payment_type_select + "' or '" + payment_type_select + "' ='All') and (SENDER= '" + sender_select + "' or '" + sender_select + "' ='All') and (RECEIVER= '" + receiver_select + "' or '"+ receiver_select +"' ='All') and (BANK= '" + bank_select + "' or '"+ bank_select +"' ='All') and   TRANS_DATE::DATE between '" + str(
                 from_dt) + "' and '" + str(to_dt) + "' group by 2,3 ").collect()
         df = pd.DataFrame(df_table3)
         if df.empty:
             st.write("No Data Found")
         else:
             d = alt.Chart(df).mark_rect().encode(
-                alt.X('TRANS_DATE:Q', bin=alt.Bin(maxbins=30)),
+                alt.X('TRANSACTION_DATE:Q', bin=alt.Bin(maxbins=30)),
                 alt.Y('TXN_COUNT:Q', bin=alt.Bin(maxbins=30)),
-                alt.Color('TXN_COUNT:Q', scale=alt.Scale(scheme='greenblue')),tooltip=['TRANS_DAT','TXN_COUNT']
+                alt.Color('TXN_COUNT:Q', scale=alt.Scale(scheme='greenblue')),tooltip=['TXN_DATE','TXN_COUNT']
                     )
             st.altair_chart(d)
 
@@ -152,98 +154,91 @@ with st.container():
 with st.container():
     st.subheader("Top 5 Senders")
     df_table3 = test_session.sql(
-            "select Top 5 SENDER,TOTAL_AMOUNT,TOTAL_AMOUNT AS AMOUNT_IN_WORDS from(select SENDER,TYPE_TRAN,SUM(AMOUNT) as TOTAL_AMOUNT  from D_FIN_STG.FIN_STG.TRANS_DTA where AMOUNT IS NOT NULL and (TYPE_TRAN= '" + payment_type_select + "' or '" + payment_type_select + "' ='All') and (SENDER= '" + sender_select + "' or '" + sender_select + "' ='All') and (RECEIVER= '" + receiver_select + "' or '"+ receiver_select +"' ='All') and (BANK= '" + bank_select + "' or '"+ bank_select +"' ='All') and  TRANS_DATE::DATE between '" + str(
+            "select Top 5 SENDER,TOTAL_AMOUNT,TOTAL_AMOUNT AS TXN_AMOUNT from(select SENDER,TYPE_TRAN,SUM(AMOUNT) as TOTAL_AMOUNT  from D_FIN_STG.FIN_STG.TRANS_DTA where AMOUNT IS NOT NULL and (TYPE_TRAN= '" + payment_type_select + "' or '" + payment_type_select + "' ='All') and (SENDER= '" + sender_select + "' or '" + sender_select + "' ='All') and (RECEIVER= '" + receiver_select + "' or '"+ receiver_select +"' ='All') and (BANK= '" + bank_select + "' or '"+ bank_select +"' ='All') and  TRANS_DATE::DATE between '" + str(
                 from_dt) + "' and '" + str(
                 to_dt) + "' group by 1,2 order by Total_Amount desc)order by Total_Amount desc").collect()
 
     df = pd.DataFrame(df_table3)
 
-    for ind in df.index:
-        df['AMOUNT_IN_WORDS'][ind] = numerize.numerize(float(df['AMOUNT_IN_WORDS'][ind]))
-
-
     if df.empty:
         st.write("No Data Found")
     else:
-        d = alt.Chart(df).mark_bar().encode(x='TOTAL_AMOUNT:Q',y="SENDER:O", tooltip=['SENDER', 'AMOUNT_IN_WORDS'], color=alt.value('#3498db')).properties(height=300, width=1100)
+        for ind in df.index:
+            df['TXN_AMOUNT'][ind] = currency + "" + numerize.numerize(float(df['TXN_AMOUNT'][ind]))
+        d = alt.Chart(df).mark_bar().encode(x='TOTAL_AMOUNT:Q',y="SENDER:O", tooltip=['SENDER', 'TXN_AMOUNT'], color=alt.value('#3498db')).properties(height=300, width=1100)
         st.altair_chart(d)
 
 with st.container():
     st.subheader("Top 5 Receivers")
     df_table3 = test_session.sql(
-            "select Top 5 RECEIVER,TOTAL_AMOUNT, TOTAL_AMOUNT AS AMOUNT_IN_WORDS from(select RECEIVER,TYPE_TRAN,SUM(AMOUNT)  as TOTAL_AMOUNT from D_FIN_STG.FIN_STG.TRANS_DTA where AMOUNT IS NOT NULL and (TYPE_TRAN= '" + payment_type_select + "' or '" + payment_type_select + "' ='All') and (SENDER= '" + sender_select + "' or '" + sender_select + "' ='All') and (RECEIVER= '" + receiver_select + "' or '"+ receiver_select +"' ='All') and (BANK= '" + bank_select + "' or '"+ bank_select +"' ='All') and  TRANS_DATE::DATE between '" + str(
+            "select Top 5 RECEIVER,TOTAL_AMOUNT, TOTAL_AMOUNT AS TXN_AMOUNT from(select RECEIVER,TYPE_TRAN,SUM(AMOUNT)  as TOTAL_AMOUNT from D_FIN_STG.FIN_STG.TRANS_DTA where AMOUNT IS NOT NULL and (TYPE_TRAN= '" + payment_type_select + "' or '" + payment_type_select + "' ='All') and (SENDER= '" + sender_select + "' or '" + sender_select + "' ='All') and (RECEIVER= '" + receiver_select + "' or '"+ receiver_select +"' ='All') and (BANK= '" + bank_select + "' or '"+ bank_select +"' ='All') and  TRANS_DATE::DATE between '" + str(
                 from_dt) + "' and '" + str(
                 to_dt) + "' group by 1,2 order by Total_Amount desc)order by Total_Amount desc").collect()
     df = pd.DataFrame(df_table3)
 
-    for ind in df.index:
-        df['AMOUNT_IN_WORDS'][ind] = numerize.numerize(float(df['AMOUNT_IN_WORDS'][ind]))
-
     if df.empty:
         st.write("No Data Found")
     else:
+        for ind in df.index:
+            df['TXN_AMOUNT'][ind] = currency + "" + numerize.numerize(float(df['TXN_AMOUNT'][ind]))
         d = alt.Chart(df).mark_bar().encode(x='TOTAL_AMOUNT:Q', y="RECEIVER:O",
-                                                tooltip=['RECEIVER', 'AMOUNT_IN_WORDS'],
+                                                tooltip=['RECEIVER', 'TXN_AMOUNT'],
                                                 color=alt.value('#3498db')).properties(height=300, width=1100)
         st.altair_chart(d)
 
 with st.container():
     st.subheader("Top 5 Banks")
     df_table3 = test_session.sql(
-            "select BANK,TYPE_TRAN,COUNT(TYPE_TRAN) AS TXN_COUNT,SUM(AMOUNT) as TOTAL_AMOUNT,SUM(AMOUNT) as AMOUNT_IN_WORDS from D_FIN_STG.FIN_STG.TRANS_DTA  where BANK in(Select BANK from(select Top 5 BANK,TYPE_TRAN,TOTAL_AMOUNT from(select BANK,TYPE_TRAN,SUM(AMOUNT)  as TOTAL_AMOUNT from D_FIN_STG.FIN_STG.TRANS_DTA where (TYPE_TRAN= '" + payment_type_select + "' or '" + payment_type_select + "' ='All') and (SENDER= '" + sender_select + "' or '" + sender_select + "' ='All') and (RECEIVER= '" + receiver_select + "' or '"+ receiver_select +"' ='All') and (BANK= '" + bank_select + "' or '"+ bank_select +"' ='All') and  TRANS_DATE::DATE between '" + str(
+            "select BANK,TYPE_TRAN AS TRANSACTION_TYPE,COUNT(TYPE_TRAN) AS TXN_COUNT,SUM(AMOUNT) as TOTAL_AMOUNT,SUM(AMOUNT) as TXN_AMOUNT from D_FIN_STG.FIN_STG.TRANS_DTA  where BANK in(Select BANK from(select Top 5 BANK,TYPE_TRAN,TOTAL_AMOUNT from(select BANK,TYPE_TRAN,SUM(AMOUNT)  as TOTAL_AMOUNT from D_FIN_STG.FIN_STG.TRANS_DTA where (TYPE_TRAN= '" + payment_type_select + "' or '" + payment_type_select + "' ='All') and (SENDER= '" + sender_select + "' or '" + sender_select + "' ='All') and (RECEIVER= '" + receiver_select + "' or '"+ receiver_select +"' ='All') and (BANK= '" + bank_select + "' or '"+ bank_select +"' ='All') and  TRANS_DATE::DATE between '" + str(
                 from_dt) + "' and '" + str(
                 to_dt) + "' group by 1,2 order by Total_Amount desc)order by Total_Amount desc)group by 1)group by 1,2").collect()
     df = pd.DataFrame(df_table3)
 
-
-    for ind in df.index:
-        df['AMOUNT_IN_WORDS'][ind] = numerize.numerize(float(df['AMOUNT_IN_WORDS'][ind]))
-
     if df.empty:
         st.write("No Data Found")
     else:
+        for ind in df.index:
+            df['TXN_AMOUNT'][ind] = currency + "" + numerize.numerize(float(df['TXN_AMOUNT'][ind]))
         d = alt.Chart(df).mark_bar().encode(
                 x='BANK',
                 y='TXN_COUNT',
-                color='TYPE_TRAN',tooltip=['BANK', 'AMOUNT_IN_WORDS','TYPE_TRAN','TXN_COUNT'],
+                color='TRANSACTION_TYPE',tooltip=['BANK', 'TXN_AMOUNT','TRANSACTION_TYPE','TXN_COUNT'],
             ).properties(height=400, width=1100)
         st.altair_chart(d)
 
 with st.container():
     st.subheader("YoY Comparison")
 
-    df_table3 = test_session.sql("select BANK,YEAR(TRANS_DATE::DATE) AS YEAR_VAL,sum(amount) AS TOTAL_AMOUNT,sum(amount) AS AMOUNT_IN_WORDS from D_FIN_STG.FIN_STG.TRANS_DTA where YEAR(TRANS_DATE::DATE)= YEAR('"+str(from_dt)+"'::DATE)  and BANK in(Select BANK from(select Top 5 BANK,TYPE_TRAN,TOTAL_AMOUNT from(select BANK,TYPE_TRAN,SUM(AMOUNT)  as TOTAL_AMOUNT from D_FIN_STG.FIN_STG.TRANS_DTA where (TYPE_TRAN= '" + payment_type_select + "' or '" + payment_type_select + "' ='All') and (SENDER= '" + sender_select + "' or '" + sender_select + "' ='All') and (RECEIVER= '" + receiver_select + "' or '"+ receiver_select +"' ='All') and (BANK= '" + bank_select + "' or '"+ bank_select +"' ='All') and  YEAR(TRANS_DATE::DATE)= YEAR('"+str(from_dt)+"'::DATE) group by 1,2 order by Total_Amount desc)order by Total_Amount desc)group by 1) group by 1,2 UNION select BANK,YEAR(TRANS_DATE::DATE),sum(amount)  AS TOTAL_AMOUNT, sum(amount) AS AMOUNT_IN_WORDS from D_FIN_STG.FIN_STG.TRANS_DTA where YEAR(TRANS_DATE::DATE)= YEAR('"+str(from_dt)+"'::DATE)-1  and BANK in(Select BANK from(select Top 5 BANK,TYPE_TRAN,TOTAL_AMOUNT from(select BANK,TYPE_TRAN,SUM(AMOUNT)  as TOTAL_AMOUNT from D_FIN_STG.FIN_STG.TRANS_DTA where (TYPE_TRAN= '" + payment_type_select + "' or '" + payment_type_select + "' ='All') and (SENDER= '" + sender_select + "' or '" + sender_select + "' ='All') and (RECEIVER= '" + receiver_select + "' or '"+ receiver_select +"' ='All') and (BANK= '" + bank_select + "' or '"+ bank_select +"' ='All') and YEAR(TRANS_DATE::DATE)= YEAR('"+str(from_dt)+"'::DATE) group by 1,2 order by Total_Amount desc)order by Total_Amount desc)group by 1)group by 1,2").collect()
+    df_table3 = test_session.sql("select BANK,YEAR(TRANS_DATE::DATE) AS YEAR,sum(amount) AS TOTAL_AMOUNT,sum(amount) AS TXN_AMOUNT from D_FIN_STG.FIN_STG.TRANS_DTA where YEAR(TRANS_DATE::DATE)= YEAR('"+str(from_dt)+"'::DATE)  and BANK in(Select BANK from(select Top 5 BANK,TYPE_TRAN,TOTAL_AMOUNT from(select BANK,TYPE_TRAN,SUM(AMOUNT)  as TOTAL_AMOUNT from D_FIN_STG.FIN_STG.TRANS_DTA where (TYPE_TRAN= '" + payment_type_select + "' or '" + payment_type_select + "' ='All') and (SENDER= '" + sender_select + "' or '" + sender_select + "' ='All') and (RECEIVER= '" + receiver_select + "' or '"+ receiver_select +"' ='All') and (BANK= '" + bank_select + "' or '"+ bank_select +"' ='All') and  YEAR(TRANS_DATE::DATE)= YEAR('"+str(from_dt)+"'::DATE) group by 1,2 order by Total_Amount desc)order by Total_Amount desc)group by 1) group by 1,2 UNION select BANK,YEAR(TRANS_DATE::DATE) AS YEAR,sum(amount)  AS TOTAL_AMOUNT, sum(amount) AS TXN_AMOUNT from D_FIN_STG.FIN_STG.TRANS_DTA where YEAR(TRANS_DATE::DATE)= YEAR('"+str(from_dt)+"'::DATE)-1  and BANK in(Select BANK from(select Top 5 BANK,TYPE_TRAN,TOTAL_AMOUNT from(select BANK,TYPE_TRAN,SUM(AMOUNT)  as TOTAL_AMOUNT from D_FIN_STG.FIN_STG.TRANS_DTA where (TYPE_TRAN= '" + payment_type_select + "' or '" + payment_type_select + "' ='All') and (SENDER= '" + sender_select + "' or '" + sender_select + "' ='All') and (RECEIVER= '" + receiver_select + "' or '"+ receiver_select +"' ='All') and (BANK= '" + bank_select + "' or '"+ bank_select +"' ='All') and YEAR(TRANS_DATE::DATE)= YEAR('"+str(from_dt)+"'::DATE) group by 1,2 order by Total_Amount desc)order by Total_Amount desc)group by 1)group by 1,2").collect()
     df = pd.DataFrame(df_table3)
-
-    for ind in df.index:
-        df['AMOUNT_IN_WORDS'][ind] = numerize.numerize(float(df['AMOUNT_IN_WORDS'][ind]))
 
     if df.empty:
         st.write("No Data Found")
     else:
+        for ind in df.index:
+            df['TXN_AMOUNT'][ind] = currency + "" + numerize.numerize(float(df['TXN_AMOUNT'][ind]))
         d=alt.Chart(df).mark_bar().encode(
-        x='YEAR_VAL:O',
+        x='YEAR:O',
         y='TOTAL_AMOUNT:Q',
-        color='YEAR_VAL:N',
-        column='BANK:N', tooltip=['YEAR_VAL', 'BANK','AMOUNT_IN_WORDS']
+        color='YEAR:N',
+        column='BANK:N', tooltip=['YEAR', 'BANK','TXN_AMOUNT']
         ).properties(height=450, width=200)
         st.altair_chart(d)
 
 with st.container():
     st.subheader("Distribution by Banks")
     df_table3 = test_session.sql(
-                "select CASE WHEN bank IS NULL THEN 'OTHERS' ELSE BANK END AS BANK,sum(amount) as TOTAL_AMOUNT,sum(amount) as AMOUNT_IN_WORDS,count(TYPE_TRAN) as TXN_COUNT from D_FIN_STG.FIN_STG.TRANS_DTA "
+                "select CASE WHEN bank IS NULL THEN 'OTHERS' ELSE BANK END AS BANK,sum(amount) as TOTAL_AMOUNT,sum(amount) as TXN_AMOUNT,count(TYPE_TRAN) as TXN_COUNT from D_FIN_STG.FIN_STG.TRANS_DTA "
                 "WHERE TRANS_DATE::DATE between '" + str(
                     from_dt) + "' and '" + str(
                     to_dt) + "' and (TYPE_TRAN= '" + payment_type_select + "' or '"+ payment_type_select +"' ='All') and (SENDER= '" + sender_select + "' or '"+ sender_select +"' ='All') and (RECEIVER= '" + receiver_select + "' or '"+ receiver_select +"' ='All') and (BANK= '" + bank_select + "' or '"+ bank_select +"' ='All') group by 1 order by 1").collect()
     df = pd.DataFrame(df_table3)
 
-    for ind in df.index:
-        df['AMOUNT_IN_WORDS'][ind] = numerize.numerize(float(df['AMOUNT_IN_WORDS'][ind]))
-
-
     if df.empty:
         st.write("No Data Found")
     else:
-        d = alt.Chart(df).mark_bar().encode(x='BANK', y='TOTAL_AMOUNT', tooltip=['BANK', 'AMOUNT_IN_WORDS','TXN_COUNT'],color=alt.value('#3498db')).properties(height=450, width=1100)
+        for ind in df.index:
+            df['TXN_AMOUNT'][ind] = currency + "" + numerize.numerize(float(df['TXN_AMOUNT'][ind]))
+        d = alt.Chart(df).mark_bar().encode(x='BANK', y='TOTAL_AMOUNT', tooltip=['BANK', 'TXN_AMOUNT','TXN_COUNT'],color=alt.value('#3498db')).properties(height=450, width=1100)
         st.altair_chart(d)
+
